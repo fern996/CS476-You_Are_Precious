e6e1a2f3e2c7bb827bfd969723cbf2ca9d0e2c80
hsm022 <hminhas11@gmail.com>
2021-02-27 14:01:50 -0600

    Adding the GetHelp view page

---
+++ you-are-precious/src/views/getHelp/GetHelp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>   <!-- HTML portion -->
+        <div class="row">
+            <div class="col hide-on-med-and-down offset-l1 l2">
+                <UserNavBar />
+            </div>
+            <div class="col  s12 l9">
+                    <GetHelpForm />
+            </div>
+        </div>
+        <hr />
+        <InfoBox />
+    </div>
+</template>
+
+<script> //javascript portion
+//import component
+import GetHelpForm from '@/components/forms/GetHelpForm.vue'
+import UserNavBar from '../../components/nav/UserNavBar'
+import InfoBox from '../../components/infobox/InfoBox'
+
+//export to HTML portion
+export default {
+    name:"getHelp",
+    components:{UserNavBar, InfoBox, GetHelpForm},
+    setup(){   
+    }
+}
+</script>
+
+<style>
+
+</style>
