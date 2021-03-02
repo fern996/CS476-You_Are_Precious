d79b8949ae2b014d33c967cf31aa88b13da9d19c
hsm022 <hminhas11@gmail.com>
2021-03-01 20:07:27 -0600

    Adding the welcome page to the project

---
+++ you-are-precious/src/views/welcome/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="welcome">
+    <InfoBox />
+    <ParallaxImage />
+    <InfoBox />
+    <ParallaxImage />
+    <InfoBox />
+    <ParallaxImage />
+  </div>  
+</template>
+
+<script>
+//import components
+import InfoBox from '../../components/infobox/InfoBox'
+import ParallaxImage from '../../components/parallax/ParallaxImage'
+export default {
+  name:"Welcome",
+  components:{InfoBox, ParallaxImage}
+
+}
+</script>
+
+<style>
+
+</style>
