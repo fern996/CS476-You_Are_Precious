32941793caf1907ddfbae8310ad02b0c063a2a28
Shawn Fernandes <shawnfernandes0912@gmail.com>
2021-03-15 12:17:37 -0600

    Adding all posts, comments and classifications

---
+++ you-are-precious/src/components/abuse/Classifications.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Classification 1</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in vestibulum orci. Vivamus accumsan ac nulla ac egestas. Vivamus vitae luctus arcu. Maecenas accumsan 
+        bibendum eleifend. In turpis nisi, accumsan et porta in, porttitor id enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Integer quis ante fermentum, malesuada quam sed, dictum elit. Nulla a sollicitudin ex. Sed porta, enim sed fermentum feugiat, justo felis volutpat velit, at maximus
+         magna ligula non augue.</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Classification 2</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in vestibulum orci. Vivamus accumsan ac nulla ac egestas. Vivamus vitae luctus arcu. Maecenas accumsan 
+        bibendum eleifend. In turpis nisi, accumsan et porta in, porttitor id enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Integer quis ante fermentum, malesuada quam sed, dictum elit. Nulla a sollicitudin ex. Sed porta, enim sed fermentum feugiat, justo felis volutpat velit, at maximus
+         magna ligula non augue.</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Classification 3</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in vestibulum orci. Vivamus accumsan ac nulla ac egestas. Vivamus vitae luctus arcu. Maecenas accumsan 
+        bibendum eleifend. In turpis nisi, accumsan et porta in, porttitor id enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Integer quis ante fermentum, malesuada quam sed, dictum elit. Nulla a sollicitudin ex. Sed porta, enim sed fermentum feugiat, justo felis volutpat velit, at maximus
+         magna ligula non augue.</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Classification 4</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in vestibulum orci. Vivamus accumsan ac nulla ac egestas. Vivamus vitae luctus arcu. Maecenas accumsan 
+        bibendum eleifend. In turpis nisi, accumsan et porta in, porttitor id enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Integer quis ante fermentum, malesuada quam sed, dictum elit. Nulla a sollicitudin ex. Sed porta, enim sed fermentum feugiat, justo felis volutpat velit, at maximus
+         magna ligula non augue.</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Classification 5</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in vestibulum orci. Vivamus accumsan ac nulla ac egestas. Vivamus vitae luctus arcu. Maecenas accumsan 
+        bibendum eleifend. In turpis nisi, accumsan et porta in, porttitor id enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Integer quis ante fermentum, malesuada quam sed, dictum elit. Nulla a sollicitudin ex. Sed porta, enim sed fermentum feugiat, justo felis volutpat velit, at maximus
+         magna ligula non augue.</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Classification 6</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in vestibulum orci. Vivamus accumsan ac nulla ac egestas. Vivamus vitae luctus arcu. Maecenas accumsan 
+        bibendum eleifend. In turpis nisi, accumsan et porta in, porttitor id enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Integer quis ante fermentum, malesuada quam sed, dictum elit. Nulla a sollicitudin ex. Sed porta, enim sed fermentum feugiat, justo felis volutpat velit, at maximus
+         magna ligula non augue.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ you-are-precious/src/components/comment/CommentList.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="collection">
+      <li v-for="comment in comments" :key="comment.id" class="collection-item">
+        <SignleComment :comment="comment"/>
+      </li>
+    </ul>
+  
+</template>
+
+<script>
+import SignleComment from './SingleComment'
+export default {
+    props: ['comments'],
+    components: {SignleComment},
+    setup(props){
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
